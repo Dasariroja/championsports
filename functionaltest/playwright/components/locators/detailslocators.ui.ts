ebcd2a519149c31fb3e4c05c960fa934607035b2
Team-testum <testers.testum@gmail.com>
2024-04-13 15:22:11 +0100

    Updated small piece

--- functionaltest/playwright/components/locators/detailslocators.ui.ts
+++ functionaltest/playwright/components/locators/detailslocators.ui.ts
@@ -14,6 +14,7 @@ readonly searchenginebtn: Locator;
 readonly productinterest: Locator;
 readonly comments: Locator;
 readonly sent: Locator;
+readonly closebtn: Locator;
 
 constructor(page:Page){
     this.firstname = page.getByPlaceholder('First Name');
@@ -29,5 +30,6 @@ constructor(page:Page){
     this.productinterest = page.getByLabel('Sportsbook');
     this.comments= page.getByPlaceholder('Please leave your comments...');
     this.sent = page.getByText('send');
+    this.closebtn = page.locator('a').first();
 }
 }
